c24fa7329ac6fedc2b107b44cfb5d13e68113f62
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-02-09 22:25:13 +0100

    feat> inequality constraints.

--- hqp/library.cpp
+++ hqp/library.cpp
@@ -13,4 +13,31 @@ namespace hqp
         return { Eigen::MatrixXd::Identity(2, 2), Eigen::VectorXd::Ones(2) };
     }
 
+    Return Task2::run()
+    {
+        Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2, 2);
+        Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
+        A << -0.1, 1, -1, 1;
+        b << 0.55, -1.5;
+        return { A, b };
+    }
+
+    Return Task3::run()
+    {
+        Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2, 2);
+        Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
+        A << 1, 0, 1, 1;
+        b << 2.5, 2;
+        return { A, b };
+    }
+
+    Return Task4::run()
+    {
+        Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2, 2);
+        Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
+        A << 1, 0, 0, 1;
+        b << 0, 0;
+        return { A, b };
+    }
+
 } // namespace hqp
--- hqp/library.hpp
+++ hqp/library.hpp
@@ -25,6 +25,7 @@ namespace hqp
         {
             std::tie(matrix_, vector_) = std::apply(map_.first, map_.second);
             assert(matrix_.rows() == vector_.rows());
+            assert(is_equality_.size() == vector_.rows());
             is_computed_ = true;
         }
     };
@@ -42,11 +43,63 @@ namespace hqp
         {
             std::tie(matrix_, vector_) = std::apply(map_.first, map_.second);
             assert(matrix_.rows() == vector_.rows());
+            assert(is_equality_.size() == vector_.rows());
             is_computed_ = true;
         }
         void update(int a, double b)
         {
             map_.second = std::make_tuple();
+        }
+    };
+
+    class Task2 : public Task
+    {
+    private:
+        Map<> map_;
+        static Return run();
+
+    public:
+        Task2() : Task(), map_{Map<>(run, std::make_tuple())} {}
+        void compute() override
+        {
+            std::tie(matrix_, vector_) = std::apply(map_.first, map_.second);
+            assert(matrix_.rows() == vector_.rows());
+            assert(is_equality_.size() == vector_.rows());
+            is_computed_ = true;
+        }
+    };
+
+    class Task3 : public Task
+    {
+    private:
+        Map<> map_;
+        static Return run();
+
+    public:
+        Task3() : Task(), map_{Map<>(run, std::make_tuple())} {}
+        void compute() override
+        {
+            std::tie(matrix_, vector_) = std::apply(map_.first, map_.second);
+            assert(matrix_.rows() == vector_.rows());
+            assert(is_equality_.size() == vector_.rows());
+            is_computed_ = true;
+        }
+    };
+
+    class Task4 : public Task
+    {
+    private:
+        Map<> map_;
+        static Return run();
+
+    public:
+        Task4() : Task(), map_{Map<>(run, std::make_tuple())} {}
+        void compute() override
+        {
+            std::tie(matrix_, vector_) = std::apply(map_.first, map_.second);
+            assert(matrix_.rows() == vector_.rows());
+            assert(is_equality_.size() == vector_.rows());
+            is_computed_ = true;
         }
     };
 
