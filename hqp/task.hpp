f7f09271855e808643d0ab4487a14e318ff8ab8a
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-02-09 22:25:13 +0100

    feat> inequality constraints.

--- hqp/task.hpp
+++ hqp/task.hpp
@@ -17,15 +17,21 @@ namespace hqp
         Eigen::Array<bool, Eigen::Dynamic, 1> is_equality_;
         Eigen::Array<bool, Eigen::Dynamic, 1> is_active_;
         Eigen::VectorXd slack_;
+        Eigen::VectorXd dual_;
+        Eigen::Array<bool, Eigen::Dynamic, 1> is_locked_;
+        Eigen::Array<bool, Eigen::Dynamic, 1> is_free_;
         uint rank_;
         Eigen::MatrixXd codMid_;
         Eigen::MatrixXd codLeft_;
+
         Task() {}
         virtual void compute() = 0;
-        Eigen::MatrixXd get_matrix() { if (!is_computed_) compute(); return matrix_; }
-        Eigen::VectorXd get_vector() { if (!is_computed_) compute(); return vector_; }
+        Eigen::MatrixXd get_matrix();
+        Eigen::VectorXd get_vector();
         // TODO: add set_equality and other methods.
-        // bool set_equality(const Eigen::VectorXi &is_equality) { is_equality_ = is_equality; }
+        bool set_equality(const Eigen::Array<bool, Eigen::Dynamic, 1> &);
+        // bool is_active() {return is_active_.any();};
+        // bool is_active(uint row) {return is_active_(row);};
     };
 
 } // namespace hqp
