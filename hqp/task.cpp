00bed2f5011068d3cf620638be7f910a5ac69029
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-02-15 17:29:24 +0100

    chore> enforcing naming convention.

--- hqp/task.cpp
+++ hqp/task.cpp
@@ -5,25 +5,25 @@ namespace hqp
 
     Eigen::MatrixXd Task::get_matrix()
     {
-        if (!is_computed_) compute();
+        if (!isComputed_) compute();
         return matrix_;
     }
 
     Eigen::VectorXd Task::get_vector()
     {
-        if (!is_computed_)
+        if (!isComputed_)
         {
             compute();
-            if (has_guess_) vector_ -= matrix_ * guess_; // Shift problem to the origin
+            if (hasGuess_) vector_ -= matrix_ * guess_; // Shift problem to the origin
         }
         return vector_;
     }
 
-    bool Task::set_equality(const Eigen::Array<bool, Eigen::Dynamic, 1>& is_equality)
+    bool Task::set_equality(const Eigen::Array<bool, Eigen::Dynamic, 1>& equalitySet)
     {
-        auto m = is_equality.size();
-        is_equality_ = is_equality;
-        is_locked_ = is_free_ = Eigen::VectorXi::Zero(m).cast<bool>();
+        auto m = equalitySet.size();
+        equalitySet_ = equalitySet;
+        lockedSet_ = workSet_ = Eigen::VectorXi::Zero(m).cast<bool>();
         slack_ = dual_ = Eigen::VectorXd::Zero(m);
         codMid_ = codLeft_ = Eigen::MatrixXd::Zero(m, m);
         return true;
@@ -31,7 +31,7 @@ namespace hqp
 
     bool Task::set_guess(const Eigen::VectorXd& guess)
     {
-        has_guess_ = true;
+        hasGuess_ = true;
         guess_ = guess;
         return true;
     }
