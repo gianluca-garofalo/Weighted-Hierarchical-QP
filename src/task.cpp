0015fee052109ad95462c26d0073a7fabea47319
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-03-04 08:02:45 +0100

    feat> weight subtasks within a priority level.

--- src/task.cpp
+++ src/task.cpp
@@ -10,6 +10,24 @@ namespace hqp
         lockedSet_ = workSet_ = Eigen::VectorXi::Zero(m).cast<bool>();
         slack_ = dual_ = Eigen::VectorXd::Zero(m);
         codMid_ = codLeft_ = Eigen::MatrixXd::Zero(m, m);
+        weight_ = Eigen::LLT<Eigen::MatrixXd> (Eigen::MatrixXd::Identity(m, m));
+    }
+
+
+    void Task::set_weight(const Eigen::MatrixXd& weight)
+    {
+        Eigen::LLT<Eigen::MatrixXd> lltOf(weight);
+        assert(weight.isApprox(weight.transpose()) && lltOf.info() != Eigen::NumericalIssue);
+
+        if (isComputed_)
+        {
+            weight_.matrixU().solveInPlace<Eigen::OnTheLeft>(matrix_);
+            weight_.matrixU().solveInPlace<Eigen::OnTheLeft>(vector_);
+            matrix_ = lltOf.matrixU() * matrix_;
+            vector_ = lltOf.matrixU() * vector_;
+        }
+
+        weight_ = lltOf;
     }
 
 } // namespace hqp
