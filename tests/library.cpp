7813c3846e4a9ad7d57578800a73bf9b7e78e444
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-03-13 08:38:20 +0100

    fix> run does not need a return type.

--- tests/library.cpp
+++ tests/library.cpp
@@ -9,36 +9,34 @@
 
 namespace hqp {
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task0::run() {
-    return {Eigen::MatrixXd::Identity(1, 1), Eigen::VectorXd::Zero(1)};
+void Task0::run() {
+    matrix_ = Eigen::MatrixXd::Identity(1, 1);
+    vector_ = Eigen::VectorXd::Zero(1);
 }
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task1::run(double const& b0, Eigen::VectorXd const& vec) {
-    return {Eigen::MatrixXd::Identity(2, 2), b0 * vec};
+void Task1::run(double const& b0, Eigen::VectorXd const& vec) {
+    matrix_ = Eigen::MatrixXd::Identity(2, 2);
+    vector_ = b0 * vec;
 }
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task2::run() {
-    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2, 2);
-    Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
-    A << -0.1, 1, -1, 1;
-    b << 0.55, -1.5;
-    return {A, b};
+void Task2::run() {
+    matrix_ = (Eigen::MatrixXd(2, 2) << -0.1, 1, -1, 1).finished();
+    vector_ = (Eigen::VectorXd(2) << 0.55, -1.5).finished();
 }
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task3::run() {
-    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2, 2);
-    Eigen::VectorXd b = Eigen::VectorXd::Zero(2);
-    A << 1, 0, 1, 1;
-    b << 2.5, 2;
-    return {A, b};
+void Task3::run() {
+    matrix_ = (Eigen::MatrixXd(2, 2) << 1, 0, 1, 1).finished();
+    vector_ = (Eigen::VectorXd(2) << 2.5, 2.5).finished();
 }
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task5::run() {
-    return {(Eigen::MatrixXd(1, 2) << 1, 0).finished(), Eigen::VectorXd::Zero(1)};
+void Task5::run() {
+    matrix_ = (Eigen::MatrixXd(1, 2) << 1, 0).finished();
+    vector_ = Eigen::VectorXd::Zero(1);
 }
 
-std::tuple<Eigen::MatrixXd, Eigen::VectorXd> Task6::run() {
-    return {(Eigen::MatrixXd(1, 2) << 0, 1).finished(), Eigen::VectorXd::Zero(1)};
+void Task6::run() {
+    matrix_ = (Eigen::MatrixXd(1, 2) << 0, 1).finished();
+    vector_ = Eigen::VectorXd::Zero(1);
 }
 
 }  // namespace hqp
