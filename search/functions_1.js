8993184bd3e9766cec62577783b8fbe5916e5ce4
gianluca-garofalo <56539263+gianluca-garofalo@users.noreply.github.com>
2025-05-31 15:51:57 +0000

    Deploying to gh-pages from @ gianluca-garofalo/Weighted-Hierarchical-QP@72faa3683133d7ee909c9da574a1ffbf9d8d9dc6 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['dual_5fupdate_0',['dual_update',['../classhqp_1_1HierarchicalQP.html#a7234e6ffbcaa28f908a0d728e4506ca3',1,'hqp::HierarchicalQP']]]
+  ['dual_5fupdate_0',['dual_update',['../classhqp_1_1HierarchicalQP.html#ac041dd203ebdff95ae64f8371180095f',1,'hqp::HierarchicalQP']]]
 ];
