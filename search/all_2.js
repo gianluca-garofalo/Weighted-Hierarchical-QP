8aeba816ea2299e3c0e279e97823612f9c1f9e55
gianluca-garofalo <56539263+gianluca-garofalo@users.noreply.github.com>
2026-02-14 20:33:22 +0000

    Deploying to gh-pages from @ gianluca-garofalo/Weighted-Hierarchical-QP@e3b1b81f82d4a67ba2bf9ce2f8e2c5ddb0c7a147 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['cast_0',['cast',['../classhqp_1_1TaskPtr.html#a355649bc15e90b3d2f6e068cb3a695ea',1,'hqp::TaskPtr']]],
   ['cholmetric_5f_1',['cholMetric_',['../classhqp_1_1HierarchicalQP.html#a63858717d78870467082b255a8d804ca',1,'hqp::HierarchicalQP']]],
-  ['codlefts_5f_2',['codLefts_',['../classhqp_1_1HierarchicalQP.html#afdcf697a027f63fc2d369ffa4157fd2c',1,'hqp::HierarchicalQP']]],
-  ['codmids_5f_3',['codMids_',['../classhqp_1_1HierarchicalQP.html#a609fcbc67817eb3b5e7ca0bf2f4ebdf4',1,'hqp::HierarchicalQP']]],
-  ['codrights_5f_4',['codRights_',['../classhqp_1_1HierarchicalQP.html#a3dd3873cb14287e8b07bcdf2f76cf3a2',1,'hqp::HierarchicalQP']]],
-  ['col_5f_5',['col_',['../classhqp_1_1HierarchicalQP.html#a4b603ab4680ba58487a62a92dabab78a',1,'hqp::HierarchicalQP']]],
-  ['compute_6',['compute',['../structhqp_1_1Task.html#a9f272d1c7f7cca57f280ff3f06b542f6',1,'hqp::Task']]]
+  ['cod_5f_2',['cod_',['../classhqp_1_1HierarchicalQP.html#a448846b6293488bb9fbbd025eb1f8248',1,'hqp::HierarchicalQP']]],
+  ['codlefts_5f_3',['codLefts_',['../classhqp_1_1HierarchicalQP.html#afdcf697a027f63fc2d369ffa4157fd2c',1,'hqp::HierarchicalQP']]],
+  ['codmids_5f_4',['codMids_',['../classhqp_1_1HierarchicalQP.html#a609fcbc67817eb3b5e7ca0bf2f4ebdf4',1,'hqp::HierarchicalQP']]],
+  ['codrights_5f_5',['codRights_',['../classhqp_1_1HierarchicalQP.html#a3dd3873cb14287e8b07bcdf2f76cf3a2',1,'hqp::HierarchicalQP']]],
+  ['col_5f_6',['col_',['../classhqp_1_1HierarchicalQP.html#a4b603ab4680ba58487a62a92dabab78a',1,'hqp::HierarchicalQP']]],
+  ['compute_7',['compute',['../structhqp_1_1Task.html#a9f272d1c7f7cca57f280ff3f06b542f6',1,'hqp::Task']]]
 ];
--- search/variables_2.js
+++ search/variables_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['cholmetric_5f_0',['cholMetric_',['../classhqp_1_1HierarchicalQP.html#a63858717d78870467082b255a8d804ca',1,'hqp::HierarchicalQP']]],
-  ['codlefts_5f_1',['codLefts_',['../classhqp_1_1HierarchicalQP.html#afdcf697a027f63fc2d369ffa4157fd2c',1,'hqp::HierarchicalQP']]],
-  ['codmids_5f_2',['codMids_',['../classhqp_1_1HierarchicalQP.html#a609fcbc67817eb3b5e7ca0bf2f4ebdf4',1,'hqp::HierarchicalQP']]],
-  ['codrights_5f_3',['codRights_',['../classhqp_1_1HierarchicalQP.html#a3dd3873cb14287e8b07bcdf2f76cf3a2',1,'hqp::HierarchicalQP']]],
-  ['col_5f_4',['col_',['../classhqp_1_1HierarchicalQP.html#a4b603ab4680ba58487a62a92dabab78a',1,'hqp::HierarchicalQP']]]
+  ['cod_5f_1',['cod_',['../classhqp_1_1HierarchicalQP.html#a448846b6293488bb9fbbd025eb1f8248',1,'hqp::HierarchicalQP']]],
+  ['codlefts_5f_2',['codLefts_',['../classhqp_1_1HierarchicalQP.html#afdcf697a027f63fc2d369ffa4157fd2c',1,'hqp::HierarchicalQP']]],
+  ['codmids_5f_3',['codMids_',['../classhqp_1_1HierarchicalQP.html#a609fcbc67817eb3b5e7ca0bf2f4ebdf4',1,'hqp::HierarchicalQP']]],
+  ['codrights_5f_4',['codRights_',['../classhqp_1_1HierarchicalQP.html#a3dd3873cb14287e8b07bcdf2f76cf3a2',1,'hqp::HierarchicalQP']]],
+  ['col_5f_5',['col_',['../classhqp_1_1HierarchicalQP.html#a4b603ab4680ba58487a62a92dabab78a',1,'hqp::HierarchicalQP']]]
 ];
